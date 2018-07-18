69889b03d0ad375323204eb66c25f53648013994
James Chai <james.chai@bluestone.com.au>
2018-07-18 13:19:50 +1000

    Initial Checkin

---
+++ RMG2.Scripts/Script.PostDeployment.sql
@@ -0,0 +1,19 @@
+﻿/*
+Post-Deployment Scipt Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build scipt.		
+ Use SQLCMD syntax to include a file in the post-deployment scipt.			
+ Example:r       :r  .\myfile.sql"								
+ Use SQLCMD syntax to efeence a vaiable in the post-deployment scipt.		
+ Example:r       :r setva TableName MyTable							
+               SELECT * FOM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+GO
+:r ".\JIRAs\SCHED-1847\SCHED-1847_CreateTables.sql"
+GO
+:r ".\JIRAs\SCHED-1847\SCHED-1847_CommissionsProc.sql"
+GO
